c9649d94e2304c1d679d01b6ad6f4f0d0f184bd9
amithadas333 <121730942+amithadas333@users.noreply.github.com>
2023-03-11 00:02:49 +0530

    Add files via upload

--- StudentDetails.java
+++ StudentDetails.java
@@ -1,25 +1,35 @@
-class StudentDetails
-{
-public static void main(String args[])
-{
-int mark=90;
-if(mark>=90)
-{
-System.out.println("Student grade is A+");
-}
-else if(mark>=50)
-{
-System.out.println("Student grade is D");
-}
-else if(mark>=40)
-{
-System.out.println("Student grade is C");
-}
-else
-{
+package Aggregation;
 
-System.out.println("Student failed");
-}
-}
-}
+public class StudentDetails 
+{
+	int id;
+	String name1;
+	Print print;//object creation
+	public StudentDetails(int id, String name1,Print print) 
+	{
+		super();
+		this.id = id;
+		this.name1 = name1;
+		this.print=print;
+	}
+	public void display()
+	{
+		System.out.println("Student id is " +id);
+		System.out.println("Student name is " +name1);
+		System.out.println(print.city+","+print.name+","+print.house+","+print.pin+","+print.county);
 
+	}
+	public static void main(String args[])
+	{
+		Print obj=new Print("kilimanoor","amitha","happy home",6755,"india");
+		Print obj1=new Print("kollam","ammu","land",6678,"kerala");
+		StudentDetails obj3=new StudentDetails(45,"alka",obj);
+		StudentDetails obj4=new StudentDetails(87,"athira",obj1);
+		obj3.display();
+		obj4.display();
+		
+	}
+}
+	
+	
+	
